ac980af1b429dcd507d46f40d7260bb2fc310fef
Daniel <herrnichols@gmail.com>
2019-09-13 19:26:50 -0400

    finished

--- src/Components/ValidateError.js
+++ src/Components/ValidateError.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function ValidateError(props) {
   if (props.message) {
     return <div className="error">{props.message}</div>;
   }
   return <> </>;
+}
+
+ValidateError.propTypes = {
+  message: PropTypes.string.isRequired
 }
